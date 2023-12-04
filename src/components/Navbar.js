e2804b038ec027d2bcbe351f360796b831e2af7d
Kiarie <collinskiarie13@outlook.com>
2023-12-04 20:43:58 +0300

    Created and minimally styled the Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,6 +22,18 @@ function Navbar(){
             <div className="Photocard">
                 <img className="photo" src={collins} alt="Collins"/>
             </div>
+            <div className='intro'>
+                <h1 className='name'>Collins K. Kiarie</h1>
+                <h3 className='info'>I am a Software Developer</h3>
+            </div>
+            <div className='buttons'>
+                <div className='cvButton'>
+                    <button className='cv'> CV </button>
+                </div>
+                <div className='hireButton'>
+                    <button className='hire'> Hire Me </button>
+                </div>
+            </div>
         </div>
     );
 }
