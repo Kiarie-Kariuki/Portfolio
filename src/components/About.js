1e450c9f1bc0aede501a70fa5937238d8a6a2bcb
Kiarie <collinskiarie13@outlook.com>
2023-12-04 21:58:01 +0300

    Created the About component

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+function About(){
+    return(
+        <div className="AboutContainer">
+            <div className="head">
+                <h1>WHO AM I?</h1>
+            </div>
+            <div className="bio">
+                <p>I am  a software developer with 1 years of experience. I have worked on a variety of projects, including web <span className="sentence-space"> </span> applications, mobile applications, and desktop applications. I am proficient in a variety of programming languages,<span className="sentence-space"> </span> including JavaScript, Python, Java, and some of the frameworks sucj as React, Django, Flask and FASTAPI. I am also<span className="sentence-space"> </span> experienced in working with a variety of technologies, including HTML, CSS, and SQL. 
+ 
+ I am a highly motivated and<span className="sentence-space"> </span> results-oriented individual. I'm also a team player and I'm always willing to help others. I am confident that I can<span className="sentence-space"> </span> make a significant contribution to any team or project.<br /> <span className="sentence-space"> </span>Reach out to me and let us work together or let me build you world class efficient and scalable software to meet your needs.</p>
+            </div>
+        </div>
+    )
+}
+
+
+export default About;
