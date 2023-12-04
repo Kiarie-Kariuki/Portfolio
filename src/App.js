78c909ed384fd201f6ae7f20eb1889e1277b3e88
Kiarie <collinskiarie13@outlook.com>
2023-12-04 21:59:57 +0300

    Linked the About component to the main app for rendering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import About from './components/About';
 
 function App() {
   return (
+    <>
     <Navbar />
+    <About />
+    </>
   );
 }
 
