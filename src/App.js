a0bcd0dbb853ccafd43461287246eea8762f49a9
Kiarie <collinskiarie13@outlook.com>
2023-12-11 15:02:22 +0300

    Added the Professional card to the DOM

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import About from './components/About';
+import Professional from './components/Professional';
 
 function App() {
   return (
     <>
     <Navbar />
     <About />
+    <Professional />
     </>
   );
 }
