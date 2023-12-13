a01b8599061bdfea693bedbf285bef671422cfd7
Kiarie <collinskiarie13@outlook.com>
2023-12-13 23:43:48 +0300

    Enhanced the navbar for better user experience

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,80 +1,138 @@
-import collins from '../Images/collins.JPG'
+import collins from '../Images/collins.JPG';
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithubSquare, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
+console.log(collins);
 
-console.log (collins)
+function Navbar() {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+  const [scrollToContact, setScrollToContact] = useState(false);
 
-function Navbar(){
+  useEffect(() => {
+    const handleScroll = () => {
+        const currentScrollPos = window.scrollY;
+        const scrollingDown = currentScrollPos > prevScrollPos;
+        const scrollingUp = currentScrollPos < prevScrollPos;
+      
+        if (scrollingDown) {
+          setVisible(false);
+        } else if (scrollingUp && currentScrollPos < 460) {
+          setVisible(true);
+        }
+      
+        setPrevScrollPos(currentScrollPos);
+      };
+      
 
-    const [prevScrollPos, setPrevScrollPos] = useState(0);
-    const [visible, setVisible] = useState(true);
+    window.addEventListener('scroll', handleScroll);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const currentScrollPos = window.scrollY;
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollPos]);
 
-            setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 460); 
+  useEffect(() => {
+    if (scrollToContact) {
+      const contactElement = document.getElementById('contact');
+      if (contactElement) {
+        contactElement.scrollIntoView({ behavior: 'auto' });
+        setScrollToContact(false);
+      }
+    }
+  }, [scrollToContact]);
 
-            setPrevScrollPos(currentScrollPos);
-        };
+  const handleHireButtonClick = () => {
+    console.log('Hire button clicked');
+    setScrollToContact(true);
+  };
 
-        window.addEventListener('scroll', handleScroll);
+  const openPdfInNewTab = () => {
+    // Replace with the actual hosted path to PDF file
+    const pdfUrl = '';
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, [prevScrollPos]);
+    // Open the PDF in a new tab
+    window.open(pdfUrl, '_blank');
+  };
 
-    return(
-        <div className='NavbarContainer'>
-            <div className={`Navbar ${visible ? '' : 'scrolled'}`} id="navbar">
-                <div className='cv'>
-                    <h2>ONLINE CV</h2>
-                </div>
-                <div className="nav-icons">
-                    <ul className="icon">
-                        <li> ABOUT</li>
-                        <li>SKILL</li>
-                        <li>EXPERIENCE</li>
-                        <li>CONTACT</li>
-                    </ul>
-                </div>
-            </div>
-            <div className="Photocard">
-                <img className="photo" src={collins} alt="Collins"/>
-            </div>
-            <div className='intro'>
-                <h1 className='name'>Collins K. Kiarie</h1>
-                <h3 className='info'>SOFTWARE ENGINEER, FULLSTACK DEVELOPER</h3>
-            </div>
-            <div className='buttons'>
-                <div className='cvButton'>
-                    <button className='cv'> CV </button>
-                </div>
-                <div className='hireButton'>
-                    <button className='hire'> Hire Me </button>
-                </div>
-            </div>
-            <div className='share'>
-                <ul className='icons'>
-                    <li>
-                        <a href='https://github.com/yourusername'>
-                            <FontAwesomeIcon icon={faGithubSquare} size="3x" />
-                        </a>
-                    </li>
-                    <li>
-                        <a href='https://www.linkedin.com/in/collins-kiarie'>
-                            <FontAwesomeIcon icon={faLinkedin} size="3x" />
-                        </a>
-                    </li>
-                </ul>
-            </div>
+  const scrollToSection = (sectionId) => {
+    const sectionElement = document.getElementById(sectionId);
+    if (sectionElement) {
+      sectionElement.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+    }
+  };
+
+  return (
+    <div className='NavbarContainer' id='portfolio'>
+      <div className={`Navbar ${visible ? '' : 'scrolled'}`} id='navbar'>
+        <div className='portfolio' onClick={() => scrollToSection('portfolio')}>
+          <h2>PORTFOLIO</h2>
         </div>
-    );
+        <div className='nav-icons'>
+          <ul className='icon'>
+            <li onClick={() => scrollToSection('about')}> ABOUT</li>
+            <li onClick={() => scrollToSection('skill')}>SKILL</li>
+            <li onClick={() => scrollToSection('experience')}>EXPERIENCE</li>
+            <li onClick={() => scrollToSection('contact')}>CONTACT</li>
+          </ul>
+        </div>
+      </div>
+      <div className='Photocard'>
+        <img className='photo' src={collins} alt='Collins' />
+      </div>
+      <div className='intro'>
+        <h1 className='name'>Collins K. Kiarie</h1>
+        <h3 className='info'>SOFTWARE ENGINEER, FULLSTACK DEVELOPER</h3>
+      </div>
+      <div className='buttons'>
+        <div className='cvButton'>
+          <button
+            className='btn btn-primary cv'
+            onClick={openPdfInNewTab}
+          >
+            View My CV
+          </button>
+        </div>
+        <div className='hireButton'>
+          <button
+            className='btn btn-primary hire'
+            onClick={handleHireButtonClick}
+          >
+            Hire Me
+          </button>
+        </div>
+      </div>
+      <div className='share'>
+        <ul className='icons'>
+          <li>
+            <a
+              className='btn btn-default btn-round btn-lg btn-icon'
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://www.linkedin.com/in/collins-kiarie'
+              title='Follow me on LinkedIn'
+              data-original-title='Follow me on LinkedIn'
+            >
+              <FontAwesomeIcon icon={faLinkedin} size='2x' />
+            </a>
+          </li>
+          <li>
+            <a
+              className='btn btn-default btn-round btn-lg btn-icon'
+              href='https://github.com/Kiarie-Kariuki'
+              target='_blank'
+              rel='noopener noreferrer'
+              title='Check me out on Github'
+              data-original-title='Check me out on Github'
+            >
+              <FontAwesomeIcon icon={faGithub} size='2x' />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
-
-
